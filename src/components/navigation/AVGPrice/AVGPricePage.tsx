b0ad4bc5f4d696a2c9bf9837f336e7d9e84aac3c
MaksymTertychnyj <maka12008@ukr.net>
2022-04-13 20:47:19 +0300

    added component AVGPricePage

--- src/components/navigation/AVGPrice/AVGPricePage.tsx
+++ src/components/navigation/AVGPrice/AVGPricePage.tsx
@@ -1,16 +1,143 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ObjectType} from 'typescript';
+import APIRoutes from '../../../../android/api-service/APIRoutes';
+import APIService from '../../../../android/api-service/APIService';
+import DataResponse from '../../../../android/models/DTO/DataResponse';
+import ModelAdvertisement from '../../../../android/models/ModelAdvertisement';
+import ResponseAVG from '../../../../android/models/ResponseAVG';
+import ResponseMapper from '../../data-mappers/ResponseMapper';
+import KeyProviderContext from '../../KeyProvider/KeyProviderContext';
 import AVGPriceStyle from './AVGPriceStyle';
 
 const AVGPricePage = ({navigation, route}: any) => {
+  const {keyApi} = useContext(KeyProviderContext);
+  const [response, setResponse] = useState<ResponseAVG>(null);
+  const [dataList, setDataList] = useState<DataResponse[]>();
+  const [advertisement, setAdvertisement] = useState<ModelAdvertisement>(null);
+  const [selectedId, setSelectedId] = useState(null);
   const {sublink} = route.params;
+
+  const [city, setCity] = useState('');
+  const [region, setRegion] = useState('');
+  const [year, setYear] = useState('');
+  const [race, setRace] = useState('');
+
+  const renderItem = ({item}: any) => {
+    return <Item item={item} onPress={() => setSelectedId(item.classified)} />;
+  };
+
   useEffect(() => {
-    console.log(sublink);
+    APIService.get(APIRoutes.getRequestAVG(sublink, keyApi)).then(result => {
+      if (result) {
+        setResponse(result.data as ResponseAVG);
+      }
+    });
   }, [sublink]);
+
+  useEffect(() => {
+    if (response) {
+      setDataList(ResponseMapper(response));
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (selectedId) {
+      APIService.get(
+        APIRoutes.getResponseAdvertisement(selectedId, keyApi),
+      ).then(result => {
+        if (result) {
+          setAdvertisement(result.data as ModelAdvertisement);
+        }
+      });
+    }
+  }, [selectedId]);
+
+  useEffect(() => {
+    if (advertisement) {
+      setCity(advertisement['locationCityName' as keyof ModelAdvertisement]);
+      setRegion(
+        advertisement.stateData['regionName' as keyof ModelAdvertisement],
+      );
+      setYear(advertisement.autoData['year' as keyof ModelAdvertisement]);
+      setRace(advertisement.autoData['race' as keyof ModelAdvertisement]);
+    }
+  }, [advertisement]);
+
+  const Item = ({item, onPress}: any) => (
+    <TouchableOpacity onPress={onPress}>
+      <View style={AVGPriceStyle.itemResult}>
+        <Text style={[AVGPriceStyle.textContainerResult, {color: '#597D35'}]}>
+          Id:
+        </Text>
+        <Text style={AVGPriceStyle.textContainerResult}>
+          {item?.classified},
+        </Text>
+        <Text style={[AVGPriceStyle.textContainerResult, {color: '#597D35'}]}>
+          Price:
+        </Text>
+        <Text style={AVGPriceStyle.textContainerResult}>{item?.price}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={AVGPriceStyle.container}>
-      <Text>From avgPrice</Text>
+      <View style={AVGPriceStyle.header}>
+        <Text style={[AVGPriceStyle.textResult, {marginLeft: 0}]}>Total:</Text>
+        <Text style={AVGPriceStyle.textResult}>{response?.total}</Text>
+        <Text style={[AVGPriceStyle.textResult, {marginLeft: 70}]}>
+          Average price:
+        </Text>
+        <Text style={AVGPriceStyle.textResult}>
+          {response?.arithmeticMean?.toFixed(2)} $
+        </Text>
+      </View>
+      <SafeAreaView style={AVGPriceStyle.containerAdvert}>
+        <View style={AVGPriceStyle.headerAdvert}>
+          <View style={[AVGPriceStyle.header]}>
+            <Text style={[AVGPriceStyle.textAdvert, {fontWeight: 'bold'}]}>
+              City:
+            </Text>
+            <Text style={AVGPriceStyle.textAdvert}>{city + ','}</Text>
+            <Text style={AVGPriceStyle.textAdvert}>{region} обл. </Text>
+          </View>
+          <View style={[AVGPriceStyle.header]}>
+            <Text
+              style={[
+                AVGPriceStyle.textAdvert,
+                {marginLeft: 10, fontWeight: 'bold'},
+              ]}>
+              Year:
+            </Text>
+            <Text style={AVGPriceStyle.textAdvert}>{year}</Text>
+            <Text
+              style={[
+                AVGPriceStyle.textAdvert,
+                {marginLeft: 10, fontWeight: 'bold'},
+              ]}>
+              Race:
+            </Text>
+            <Text style={AVGPriceStyle.textAdvert}>{race}</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+      <SafeAreaView>
+        <ScrollView style={AVGPriceStyle.containerResult}>
+          <FlatList
+            data={dataList}
+            renderItem={renderItem}
+            keyExtractor={item => item?.classified as any}
+            extraData={selectedId}
+          />
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 };
--- src/components/navigation/AVGPrice/AVGPriceStyle.tsx
+++ src/components/navigation/AVGPrice/AVGPriceStyle.tsx
@@ -5,6 +5,59 @@ const AVGPriceStyle = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header: {
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginTop: 5,
+  },
+  textResult: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginLeft: 10,
+    letterSpacing: 0.25,
+    color: '#1520A6',
+  },
+  containerResult: {
+    width: '100%',
+    height: Dimensions.get('window').height / 2 - 90,
+    alignSelf: 'center',
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#D9DDDC',
+  },
+  itemResult: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderColor: '#D9DDDC',
+  },
+  textContainerResult: {
+    marginRight: 20,
+    marginTop: 15,
+    fontSize: 23,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#5DBB63',
+  },
+  containerAdvert: {
+    height: Dimensions.get('window').height / 2,
+    borderWidth: 1,
+    borderColor: '#D9DDDC',
+  },
+  headerAdvert: {
+    height: Dimensions.get('window').height / 10,
+    borderWidth: 1,
+    borderColor: '#D9DDDC',
+    elevation: 2,
+  },
+  textAdvert: {
+    fontSize: 18,
+    marginTop: 5,
+    marginLeft: 10,
+    letterSpacing: 0.25,
+  },
 });
 
 export default AVGPriceStyle;
